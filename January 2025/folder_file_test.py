9377c555f5a5e055b522f30b6028f871d00a5d49
dma40 <dma99878@gmail.com>
2025-01-13 21:18:47 -0500

    added a nested function definition
    modified f so that it now has g nested within it

--- January 2025/folder_file_test.py
+++ January 2025/folder_file_test.py
@@ -1,5 +1,9 @@
 def f(a: int, b: int) -> int:
-    return a + b
+
+    def g(b: int) -> int:
+        return b + 1
+
+    return a + g(b)
 
 if __name__ == '__main__':
     print("Hi from a folder")
